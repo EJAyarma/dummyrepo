a5bd96df0882b616628c645797f0e820fabfbdb0
Ayarmz <johnayarmahe@gmail.com>
2021-06-08 18:10:14 +0000

    added authentication and posting functionality

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::paginate(20);
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function store(Request $request) {
+        $rules = [
+            'body' => 'required'
+        ];
+
+        $this->validate($request, $rules);
+
+        $request->user()->posts()->create($request->only('body'));
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']); // name is inherited from the top one
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']); // name is inherited from the top one
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
